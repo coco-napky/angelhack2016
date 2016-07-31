c1a9e00da9a7aaf5227b856632a8d849cda3c780
coco-napky <coco.napky@gmail.com>
2016-07-31 05:40:16 -0600

    Gravity lol snapshot

---
+++ Assets/scripts/Gravity/Gravity.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Gravity : MonoBehaviour {
+
+	private PlanetAttributes planet;
+	private SphereBehaviour sphere;
+	private int counter = 0;
+	void Awake () {
+		planet = transform.parent.GetComponent<PlanetAttributes>();
+		Debug.Log(planet.center);
+	}
+
+	void FixedUpdate () {
+		if(sphere == null) return;
+
+		ApplyGravity();
+		if(sphere.attached)
+			sphere = null;
+	}
+
+	void ApplyGravity () {
+		Vector2 currentPosition = new Vector2(sphere.transform.position.x, sphere.transform.position.y);
+		float distance = Vector2.Distance(currentPosition,planet.center);
+		float angle = Angler.GetAngle(currentPosition, planet.center);
+		Vector2 direction = Angler.GetDirecton(angle, 180f);
+    sphere.rb.AddForce(direction * distance * Time.deltaTime * 10);
+		Debug.Log(distance * Time.deltaTime);
+	}
+
+	void OnTriggerEnter2D (Collider2D collider) {
+		GameObject gameObject = collider.gameObject;
+		Debug.Log("GRAVITY ENTER TO : " + gameObject.tag);
+		switch(gameObject.tag){
+			case "Player":
+				sphere = gameObject.GetComponent<SphereBehaviour>();
+			break;
+		}
+	}
+}
---
+++ Assets/scripts/Utils/Angler.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Angler : MonoBehaviour {
+
+	//Gets the angle in degrees formed by a point relative to a center
+	public static float GetAngle (Vector2 first, Vector2 center) {
+		float deltaX = first.x - center.x;
+		float deltaY = first.y - center.y;
+		return Mathf.Atan2(deltaY, deltaX) * 180/ Mathf.PI;
+	}
+
+	public static Vector3 GetDirecton (float angle, float offset) {
+		return Quaternion.AngleAxis(angle + offset, Vector3.forward) * Vector3.right;
+	}
+}
