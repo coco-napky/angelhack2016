e8ce0b4333bbc67cd023d447f348bdd79a00223c
coco-napky <coco.napky@gmail.com>
2016-08-01 03:12:21 -0600

    Planet color interpolation

--- Assets/scripts/Planet/PlanetAttributes.cs
+++ Assets/scripts/Planet/PlanetAttributes.cs
@@ -3,20 +3,23 @@ using System.Collections;
 
 public class PlanetAttributes : MonoBehaviour {
 	public float radius;
-    public Vector2 center;
-    public bool visited = false;
+  public Vector2 center;
+  public bool visited = false;
 	public bool waypoint = false;
-
+	private Renderer _renderer;
 
 	public Color waypointColor = new Color(42,221,115);
 	public int cameraDirection = 2;
 
 	//This happens before Start()
 	void Awake() {
+		_renderer = GetComponent<Renderer>();
 		SetCenter();
 		SetRadius();
+
 		if(waypoint)
-			this.SetColor (waypointColor);
+			SetColor(waypointColor);
+
 	}
 
     void FixedUpdate() {
@@ -24,18 +27,28 @@ public class PlanetAttributes : MonoBehaviour {
 	}
 
 	public void SetColor(Color color) {
-		GetComponent<Renderer>().material.color = color;
+		tweenColor(_renderer.material.color, color, 0.25f);
 	}
 
 	void SetCenter() {
-		float x = GetComponent<Renderer>().bounds.center.x,
-		      y = GetComponent<Renderer>().bounds.center.y;
+		float x = _renderer.bounds.center.x,
+		      y = _renderer.bounds.center.y;
 
 		if(x != center.x && y != center.y)
 			center  = new Vector2(x,y);
 	}
 
 	void SetRadius() {
-		radius  = GetComponent<Renderer>().bounds.size.y/2 + 0.5f;
+		radius  = _renderer.bounds.size.y/2 + 0.5f;
 	}
+
+	void UpdateColor (Color newColor) {
+		_renderer.material.color = newColor;
+	}
+
+	void tweenColor (Color from, Color to, float time) {
+		iTween.ValueTo (gameObject, iTween.Hash ("from", from, "to", to, "time",
+										time, "easetype", "easeInCubic", "onUpdate","UpdateColor"));
+	}
+
 }
