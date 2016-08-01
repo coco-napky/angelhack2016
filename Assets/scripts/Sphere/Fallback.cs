62da55ae70a0d0d120962bc3fbda2f242cc5b38d
coco-napky <coco.napky@gmail.com>
2016-08-01 02:22:19 -0600

    Deleted Repeated code from Fallback

--- Assets/scripts/Sphere/Fallback.cs
+++ Assets/scripts/Sphere/Fallback.cs
@@ -6,7 +6,6 @@ public class Fallback : MonoBehaviour {
 	public bool cooldown = false;
 	private Vector2 direction;
 	public SphereBehaviour sphere;
-
 	public Color active;
 	public Color inactive;
 
@@ -15,21 +14,10 @@ public class Fallback : MonoBehaviour {
 
 		Vector2 currentPosition = new Vector2(sphere.transform.position.x, sphere.transform.position.y);
 		Vector2 center = sphere.currentPlanet.center;
-		float angle = GetAngle(currentPosition, center);
-		direction = GetDirecton(angle);
+		float angle = Angler.GetAngle(currentPosition, center);
+		direction = Angler.GetDirecton(angle, 180f);
 		sphere.rb.velocity = direction * 10;
 		SetCooldown(true);
-	}
-
-	//Gets the angle in degrees formed by a point relative to a center
-	float GetAngle (Vector2 first, Vector2 center) {
-		float deltaX = first.x - center.x;
-		float deltaY = first.y - center.y;
-		return Mathf.Atan2(deltaY, deltaX) * 180/ Mathf.PI;
-	}
-
-	Vector3 GetDirecton (float angle) {
-		return Quaternion.AngleAxis(angle + 180f, Vector3.forward) * Vector3.right;
 	}
 
 	public void SetCooldown (bool state) {
@@ -49,4 +37,5 @@ public class Fallback : MonoBehaviour {
 		iTween.ValueTo (gameObject, iTween.Hash ("from", from, "to", to, "time",
 										time, "easetype", "easeInCubic", "onUpdate","UpdateColor"));
 	}
+
 }
